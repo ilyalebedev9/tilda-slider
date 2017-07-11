2d0445b369e4cb3ec2b68a9d26e263d1aeb7dbfc
tab <tukhfatullin@gmail.com>
2017-07-11 18:34:56 +0700

    Fix breakpoints

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
 $('.js-slider').slick({
   dots: false,
   infinite: true,
-  speed: 300,
+  speed: 500,
   slidesToShow: 2,
   slidesToScroll: 2,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1200,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
-        infinite: true,
-        dots: true
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 980,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     },
     {
